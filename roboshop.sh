21e34e28eb08ec24f5603480fc8ac99bc279ce17
ssr-devops <surendar,dev08@gmail.com>
2026-02-03 09:32:03 +0530

    cart issue

--- roboshop.sh
+++ roboshop.sh
@@ -2,8 +2,8 @@
 
 SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z05013202FKF0ZL12WAOP"
-DOMAIN_NAME="daws88s.online"
+ZONE_ID="sg-0f45cd6cf89edb6b8"
+DOMAIN_NAME="ssrdevops.online"
 
 for instance in $@
 do
